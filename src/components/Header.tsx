fff166db24c815ca9c87ff0bf1695b64165c506e
Arun Raj <arunrajwebdeveloper@gmail.com>
2026-09-24 20:40:31 +0530

    test cicd

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ const Header = () => {
   return (
     <header className="bg-primary">
       <div className="box-container h-14 flex items-center justify-between">
-        <h2 className="text-xl font-semibold text-secondary">arunraj.</h2>
+        <h2 className="text-xl font-semibold text-secondary">arunraj...</h2>
         <a
           href="."
           onClick={(e) => {
